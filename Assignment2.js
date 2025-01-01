c69d33048376dcd46109fef05d78af21d1f0f1a9
Harish <harish210430@gmail.com>
2024-12-31 22:00:43 -0800

    Add Assignment2.js and Update it

--- Assignment2.js
+++ Assignment2.js
@@ -14,8 +14,54 @@ console.log(reverse(nums));
 function arrayToObject(arr) {
     let getObj = {};
     for(let i=0; i<arr.length; i++) {
-        console.log(getObj.i);
+        getObj[i] = arr[i];
     }
-    // return getObj;
+    return getObj;
 }
 console.log(arrayToObject(nums));
+
+
+// Deep Clone the object given below using spread (...) operator also write multiple test to check if the object is deep cloned or not.
+let info = [
+    {
+      employee: 'Nicole',
+      total: 13,
+      lineItems: {
+        elements: [
+          {
+            name: 'Burger',
+            price: 8,
+          },
+          {
+            name: 'Lamb',
+            price: 6.5,
+          },
+        ],
+      },
+    },
+    {
+      employee: 'Dan',
+      total: 11,
+      lineItems: {
+        elements: [
+          {
+            name: 'Lamb',
+            price: 4.5,
+          },
+          {
+            name: 'Meatballs',
+            price: 6.5,
+          },
+        ],
+      },
+    },
+  ];
+  let personsInfo =  [{...info[0]}, {...info[1]}];
+
+//   let personsInfo1 =  [{...info[0], lineItems: {...info[0].lineItems.elements[0]}}, {...info[1], lineItems: {...info[1].lineItems.elements[0]}}];
+
+  let personsInfo2 =  [{...info[0], lineItems: {...info[0].lineItems.elements[0]}, lineItems: {...info[0].lineItems.elements[1]}}, {...info[1], lineItems: {...info[1].lineItems.elements[0]}, lineItems: {...info[1].lineItems.elements[1]}}];
+
+  console.log(personsInfo);
+//   console.log(personsInfo1);
+  console.log(personsInfo2);
