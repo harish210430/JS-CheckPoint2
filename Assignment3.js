8b643b408e5944fd2ba319942c94ad8063f6e4f9
Harish <harish210430@gmail.com>
2024-12-31 22:03:32 -0800

    Add Assignment3.js

---
+++ Assignment3.js
@@ -0,0 +1,26 @@
+/*
+Below you will find some inputs and outputs. You have to write the name of the array methods that can/should be used to get the output from the input. You don't have to write how you get the output. Just write the name and the reason why you should use the specific method.
+
+First it's input and after ==> is output.
+
+[1, 2, ,3 , 5] ==> log 1, 2, 3, 5
+
+[1, 3, 4, 5, 6] ==> [1, 3, 4, 5, 6]
+
+[1, 3, 4, 5, 6] ==> 38
+
+[1, 3, 4, 5, 6] ==> {0: 1, 1: 3, 2: 4, 3: 5, 4: 5, 5: 6}
+
+[1, 3, 4, 5, 6] ==> ['one', 'three', 'four', 'five', 'six']
+
+["work", "touched tree", "pizza", "running", "television"] ==> output is a string
+
+["work", "touched tree", "pizza", "running", "television"] ==> output is a number
+
+["work", "touched tree", "pizza", "running", "television"] ==> output is an object
+
+["work", "touched tree", "pizza", "running", "television"] ==> ['w', 't', 'p', 'r', 't']
+
+["work", "touched tree", "pizza", "running", "television"] ==> log all the elements of the array
+
+*/
